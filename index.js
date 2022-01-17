6d88e20eefa72260c6bd2f4f0eafac477c13d18f
Yan Musikhin <yanmusikhin@gmail.com>
2022-01-17 23:57:23 +0400

    Remove repeating code

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-export function findFirstAvailableLastIndex(inputString, substring1, substring2) {
-    if (inputString.match(/^$/)) {
-        return -1;
-    }
+export function findFirstAvailableLastIndex(inputString, ...substring) {
     let i = inputString.length - 1;
-    let substring1Index = -1;
-    let substring2Index = -1;
     while (i > 0) {
-        if (inputString.substring(i, i + 1) === substring1) {
-            substring1Index = i;
-            break;
-        }
-        if (inputString.substring(i, i + 1) === substring2) {
-            substring2Index = i;
-            break;
+        if (substring.includes(inputString.substring(i, i + 1))) {
+           return  i;
         }
         i = i - 1;
     }
-    if (substring1Index !== -1) {
-        if (substring2Index === -1) {
-            return substring1Index;
-        }
-        return Math.max(substring1Index, substring2Index);
-    }
-    if (substring2Index !== -1) {
-        return substring2Index;
-    }
     return -1;
-
 }
