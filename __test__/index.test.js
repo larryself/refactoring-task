e7e24ac4941cc780fb60d08f93518508afbcda94
Yan Musikhin <yanmusikhin@gmail.com>
2022-01-18 19:32:41 +0400

    change tests description

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,35 +1,41 @@
 import {findFirstAvailableLastIndex as func} from "../index";
 
 describe('findFirstAvailableLastIndex', () => {
-        it('substring2 встречается раньше substring1 при поиске с конца строки', () => {
+        it('второй искомый элемент встречается раньше первого искомого элемента', () => {
             expect(func('абвгде', 'д', 'е')).toBe(5);
         })
-        it('substring1 встречается раньше substring2 при поиске с конца строки', () => {
+        it('первый искомый элементр встречается позже второго искомого элемента', () => {
             expect(func('abcde', 'e', 'd')).toBe(4);
         })
         it('Пустая строка', () => {
             expect(func('', 'b', 'a')).toBe(-1);
         })
-        it('пустой substring1', () => {
+        it('пустой первый искомый элемент', () => {
             expect(func('abcde', '', 'b')).toBe(1);
         })
-        it('пустой substring2', () => {
+        it('пустой второй искомый элемент', () => {
             expect(func('abcde', 'b', '')).toBe(1);
         })
-        it('substring1 равен substring2', () => {
+        it('искомые элементы равны', () => {
             expect(func('abcde', 'c', 'c')).toBe(2);
         })
-        it('substring1 в начале строки', () => {
+        it('первый искомый элемент в начале строки', () => {
             expect(func('abcde', 'a', '')).toBe(-1);
         })
-        it('substring2 в начале строки', () => {
+        it('второй искомый элемент в начале строки', () => {
             expect(func('abcde', '', 'a')).toBe(-1);
         })
-        it('substring1, substring2 не содержатся в строке', () => {
+        it('искомые элементы не содержатся в стркое ', () => {
             expect(func('abcde', 'q', 'z')).toBe(-1);
         })
         it('несколько повторений искомого', () => {
             expect(func('abacdae', 'b', 'a')).toBe(5);
+        })
+        it('искомые элементы из двух и более букв', () => {
+            expect(func('hello,world', 'llo', 'wo')).toBe(-1);
+        })
+        it('первый искомый элемент из несколькимх символом , второй из одного символа', () => {
+            expect(func('hello,world', 'llo', 'w')).toBe(6);
         })
     }
 )
